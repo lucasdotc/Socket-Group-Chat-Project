a81143ce4c085d84bcb5ecf98602a8bb48fe5e02
lucascarvalho <lucas.carvalho@ucalgary.ca>
2022-02-17 21:16:55 -0700

    completed the first run of the ClientHandler class

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -15,26 +15,47 @@ public class ClientHandler implements Runnable{
         this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         this.clientName = in.readLine();
         clientHandlers.add(this);
+        announce(clientName,"[SERVER]: "+ clientName+" has joined the chat!");
+        closeAll(socket,out,in);
     }
     public void announce(String clientName, String message)throws IOException{
         for (ClientHandler clientHandler: clientHandlers){
             clientHandler.out.write(clientName + message);
             clientHandler.out.flush();
-        }
+        }closeAll(socket,out,in);
     }
     public void removeClient() throws IOException{
         clientHandlers.remove(this);
         announce(clientName, "[SERVER]:"+ clientName +" has left the chat.");
+
+        closeAll(socket,out,in);
     }
 
     public void closeAll(Socket socket, PrintWriter out, BufferedReader in) throws IOException{
         removeClient();
+        if (socket != null){
+            socket.close();
 
+        }
+        if (out != null){
+            out.close();
+        }
+        if (in != null){
+            in.close();
+        }
     }
 
 
     @Override
     public void run() {
-
+        String textFromClient;
+        while (socket.isConnected()){
+            try{
+                textFromClient = in.readLine();
+                announce(clientName, textFromClient);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
